b3fed2634f4c7f81134293780347be0c24b7ed7c
Jonathan Carpio <101604637+jona70x@users.noreply.github.com>
2023-05-19 16:05:14 -0700

    First commit 🚀/ Initial Webpack + TS Config

---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+"use strict";
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devtool: "eval",
+  mode: "development",
+  entry: {
+    main: "./src/js/index.ts",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.resolve(__dirname, "src"),
+    port: 8080,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["main"],
+    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     {
+    //       from: "node_modules/leaflet/dist/images",
+    //       to: "images",
+    //     },
+    //   ],
+    // }),
+  ],
+  module: {
+    rules: [
+      // Assets loaders
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+            },
+          },
+        ],
+        type: "asset/resource",
+
+        generator: {
+          filename: "assets/[name].[contenthash][ext]", // Specify the output path for images
+        },
+      },
+      // CSS Loader
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name].[contenthash][ext]", // Specify the output path for images
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name].[contenthash][ext]", // Specify the output path for images
+        },
+      },
+      // Ts Loader Plugin
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: "style-loader",
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: "css-loader",
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: () => [autoprefixer],
+              },
+            },
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      leaflet: path.resolve(__dirname, "node_modules/leaflet/dist/leaflet.js"),
+    },
+  },
+};
